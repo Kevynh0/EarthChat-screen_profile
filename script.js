8f81991834e737fb35df5da1e1e5f6b169869241
KEVYN DE ANDRADE LEITE <24122559@CFP127ALUNO.LOCAL>
2025-04-24 12:25:52 -0300

    Realização do JS da screen profile

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 'use stict'
 
+// Cria uma função assíncrona
+async function profile (){
+
+    const username = document.getElementById('username')
+    const perfilImg = document.getElementById('perfilImg')
+
+    // Nessa var, é necessário integrar com o id do user logado
+    let idUsuario = 1
+
+    // EndPoint que lista usuários pelo Id (perfil)
+    const dataUser = await fetch(`https://back-spider.vercel.app/user/pesquisarUser/${idUsuario}`) 
+
+    // Converte a resposta da API em JSON, armazenando os dados do usuário na variável user
+    const user = await dataUser.json()
+
+    console.log(user);
+
+    username.textContent = user.nome
+    perfilImg.src = user.imagemPerfil
+    
+
+    
+}
+
+profile()
+
+// O "use stict" é para usar nos navegadores
+// Eu sei que para usar o "await", é necessário usar o "async function"
